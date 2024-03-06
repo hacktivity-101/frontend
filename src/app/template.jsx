941844b5f83f0c8430bf0b80c243fa741ae54b09
hacktivity-101 <maddyakash2136@gmail.com>
2024-03-06 20:09:22 +0530

    react-day-5

---
+++ src/app/template.jsx
@@ -0,0 +1,19 @@
+'use client'
+import Navbar from '@/components/Navbar';
+import React, { Children, useEffect } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Template = ({children}) => {
+    useEffect(() => {
+      require('bootstrap/dist/js/bootstrap.min.js');
+    }, [])
+    
+  return (
+    <div>
+        <Navbar />
+        {children}
+        </div>
+  )
+}
+
+export default Template;
--- src/app/todolist/page.jsx
+++ src/app/todolist/page.jsx
@@ -1,78 +1,79 @@
-'use client'
+'use client';
 import React, { useState } from 'react'
 
 const TodoList = () => {
+
     const [taskList, setTaskList] = useState([
-        { text : 'Do Coding', Completed : false },
-        { text : 'Play Games', Completed : false },
-        { text : 'Bring Milk', Completed : false },
+        { text: 'Do Coding', completed: false },
+        { text: 'Play Games', completed: false },
+        { text: 'Bring Milk', completed: false },
+        { text: 'Bring Milk', completed: false },
     ]);
-    
-const addTask = (e) => {
-    if(e.code === 'Enter'){
-        console.log(e.target.value);
 
-        const obj = { text : e.target.value, completed: false};
+    const addTask = (e) => {
+        if (e.code === 'Enter') {
+            console.log(e.target.value);
 
-        setTaskList([ obj, ...taskList]);
+            const obj = { text: e.target.value, completed: false };
 
-        e.target.value = '';
+            setTaskList([obj, ...taskList]);
+
+            e.target.value = '';
+        }
     }
-}
-const deleteTask = (index) => {
-    //console.log(index);
-    const temp = taskList;
-    temp.splice(inbox, 1);
-    setTaskList([...temp]);
-}
 
-const completeTask = (index) => {
-     const temp = taskList;
-     temp[index].completed = true;
-     setTaskList([...temp])
-}
+    const deleteTask = (index) => {
+        // console.log(index);
+        const temp = taskList;
+        temp.splice(index, 1);
+        setTaskList([...temp]);
+    }
 
-  return (
-    <div className='bg-danger-subtle vh-100'>
-        <div className='container py-5'>
-            <h1 className='display-3 fw-bold text-center'>Todo List</h1>
-            <div className='card shadow'>
-                <div className='card-header py-3'>
-                    <input
-                     type="text" 
-                    className='form-control border-primary border-3'
-                    onKeyDown={addTask}
-                    
-                    />
+    const completeTask = (index) => {
+        const temp = taskList;
+        temp[index].completed = true;
+        setTaskList([...temp]);
+        console.log(temp);
+    }
+
+    return (
+        <div className='bg-primary-subtle vh-100'>
+            <div className='container py-5'>
+
+                <h1 className='display-3 fw-bold text-center'>Todo List</h1>
+
+                <div className='card shadow'>
+                    <div className='card-header py-3'>
+                        <input
+                            type="text"
+                            className='form-control border-primary border-3'
+                            onKeyDown={addTask}
+                        />
+                    </div>
+                    <div className='card-body'>
+
+                        {
+                            taskList.map((task, index) => {
+                                return <div key={index} className='mb-4 d-flex justify-content-between align-items-center'>
+                                    <p className='my-auto'>{task.text}</p>
+                                    {task.completed ?
+                                        <span className='badge bg-success'>Completed</span>
+                                        : <span className='badge bg-warning'>Pending</span>}
+                                    <button
+                                        onClick={() => { deleteTask(index) }}
+                                        className='btn btn-danger'>Delete</button>
+                                    <button
+                                        onClick={() => { completeTask(index) }}
+                                        className='btn btn-primary'>Complete Task</button>
+                                </div>
+                            })
+                        }
+
+                    </div>
                 </div>
-                <div className='card-body'>
-                    {
-                        taskList.map((task, index) => {
-                            return <div key={index} className=' mb-4 d-flex justify-content-between'> 
-                            <p className='my-auto'>{task.text}</p>
-                            {task.completed ?
-                            <span className='badge bg-success'>Completed</span>
-                          :  <span className='badge bg-warning'>Pending</span>
-                    }
-                            <button onClick={() => { deleteTask(index) }} className='btn btn-danger'>Delete</button>
-                           <button onClick={ () => {completeTask(index)} } className='btn btn-primary'>
-                            Complete Task
-                           </button>
-                            </div>
-                       
-                    })
-                    }
-
-                    
-                </div>
-
-
             </div>
-
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default TodoList;
